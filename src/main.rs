7aea652e5d0771242b2f2e630ca095c627f30462
Yummy-Yums <eliasderby90@gmail.com>
2025-09-26 16:15:01 +0000

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,6 @@ fn main() {
             Ok(stream) => {
                 println!("accepted new connection");
                 handle_stream(stream);
-
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -32,14 +31,27 @@ fn handle_stream(mut stream: TcpStream) {
     loop {
         let bytes_read = stream.read(&mut buf).expect("Failed to read from client");
         println!("read {} bytes", bytes_read);
-        println!("{}", std::str::from_utf8(&buf).unwrap());
+
+        let info: Vec<&str> = std::str::from_utf8(&buf).unwrap().split("\r\n").collect();
+        let mut http_response = info[0].split_whitespace();
+        let _ =  http_response.next();
+        let path = http_response.next();
+
         if bytes_read == 0 {
             return ;
         }
 
-        let response = b"HTTP/1.1 200 OK\r\n\r\n";
+        if path != Some("/") {
+            let response = format!("HTTP/1.1 404 Not Found\r\n\r\n");
+            stream.write(response.as_bytes()).expect("Failed to write to server");
+            break;
+        }
+
+
+        let response = b"HTTP/1.1  200 OK\r\n";
 
         stream.write_all(response).expect("Failed to write to server");
+        break;
 
     }
 }
